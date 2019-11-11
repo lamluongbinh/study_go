e2b2dce1e1a8c1105167def01d4c96aafc5bfb85
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2019-11-11 15:46:36 +0700

    Commit Ex2: Values

---
+++ 2.Values/Values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("If I have an Apple and buy 1 more, my current apple is:", 1+1)
+	fmt.Println("If I have some world like Mo and ney, when I combined it I have: " + "Mo" + "ney")
+	fmt.Println("If I want calculate some float number like 10/3:", 10.0/3.0)
+	fmt.Println("If I want calculate some Modulo like 10%3:", 10%3)
+	fmt.Println("I can calculate some boolean like true && false:", true && false)
+	fmt.Println("I can calculate some boolean like true || false:", true || false)
+	fmt.Println("I can calculate some bool ean like !false:", !false)
+}
