857ad0ada9c0a7a5b196eaefce949a3ade85e201
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2019-11-11 16:24:50 +0700

    Commit Ex3. Variables

---
+++ 3.Variables/Variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "This is a string"
+	fmt.Println(a)
+	var b, c int = 1, 2
+	fmt.Println("b = ", b, " and c = ", c)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "This is an apple"
+	fmt.Println(f)
+	g := 3
+	fmt.Println(g)
+}
