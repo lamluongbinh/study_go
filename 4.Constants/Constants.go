ea2d176feaeb5b15f7b3a8b3bd299cd462c9879e
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2019-11-14 16:40:41 +0700

    Commit Ex4. Constants

---
+++ 4.Constants/Constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const s string = "Truth"
+
+func main() {
+	fmt.Println(s, "is never changed")
+	fmt.Println("If we test to change", s, "to a Lie")
+	fmt.Println("We will get error, @@, just test by add s = \"lie\" to the code")
+	const a = 10
+	fmt.Println("Remember that number is also can be a const, just like the code above")
+}
