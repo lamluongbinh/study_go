8b9b9b2dc60642120c87bd12df63f7998cba3e73
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2020-03-03 14:19:26 +0700

    Ex11: Maps

---
+++ 10.Maps/Maps.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+	m["key1"] = 10
+	m["key2"] = 20
+	fmt.Println(m)
+	value1 := m["key1"]
+	fmt.Println("Get value from key1: ", value1)
+	fmt.Println("Size of map:", len(m))
+
+	delete(m, "key1")
+	fmt.Println("Remove key 1 from map:", m)
+
+	fmt.Printf("Checking if key is exist in map:")
+	_, prs := m["key1"]
+	fmt.Println("You can see when key1 is removed:", prs)
+	_, prs = m["key2"]
+	fmt.Println("And key2 is existing:", prs)
+
+	m2 := map[string]int{"m2key1": 100, "m2key2": 200}
+	fmt.Println("Another way to declare map:", m2)
+
+}
