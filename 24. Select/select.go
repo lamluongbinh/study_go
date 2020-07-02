f6db71cf25e6590a50c70f930934c9ad8a8fdf29
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2020-07-02 17:45:04 +0700

    24. Select

---
+++ 24. Select/select.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func reveive(ti int, c chan string, message string) {
+	time.Sleep(time.Duration(ti) * time.Second)
+	c <- message
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	ms1 := "First"
+	ms2 := "Second"
+	i1 := 1
+	i2 := 2
+	go reveive(i1, c1, ms1)
+	go reveive(i2, c2, ms2)
+	//A select without default will block code until a case statement is executed
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("Received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("Received", msg2)
+		}
+	}
+}
