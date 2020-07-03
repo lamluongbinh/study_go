001f492020ef505a1a6334dd4156fc8ebb1abca8
Binh Lam <binhlam@Binhs-MacBook-Pro.local>
2020-07-03 14:11:09 +0700

    24. Channel - add more

--- 24. Select/select.go
+++ 24. Select/select.go
@@ -19,7 +19,18 @@ func main() {
 	i2 := 2
 	go reveive(i1, c1, ms1)
 	go reveive(i2, c2, ms2)
-	//A select without default will block code until a case statement is executed
+	// A select without default will block code until a case statement is executed
+	// So with default Nothing happen will be printed
+	select {
+	case msg1 := <-c1:
+		fmt.Println("Received", msg1)
+	case msg2 := <-c2:
+		fmt.Println("Received", msg2)
+	default:
+		fmt.Println("Nothing happen")
+	}
+	// Without for, select will just happen 1 time, when a message arrived to msg
+	// So we must add for 2 times for received all
 	for i := 0; i < 2; i++ {
 		select {
 		case msg1 := <-c1:
