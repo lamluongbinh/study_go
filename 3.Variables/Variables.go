aa786fdea545b9e270954747b783e5bfe7d52365
lamluongbinh <lamluongbinh@gmail.com>
2021-04-25 12:38:45 +0700

    3. Variable - add lines

--- 3.Variables/Variables.go
+++ 3.Variables/Variables.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	var a = "This is a string"
 	fmt.Println(a)
+	var a1 string = "This is also a string"
+	fmt.Printf("This variable type is %T\n", a1)
 	var b, c int = 1, 2
 	fmt.Println("b = ", b, " and c = ", c)
 	var d = true
